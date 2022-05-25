525cd540380f59522999553e49ad95fbff2664b6
Sherifdeen Ishola <isholasherifdeen@gmail.com>
2022-05-25 20:12:31 +0000

    Completed Design

---
+++ index.js
@@ -0,0 +1,9 @@
+
+// toggle buttons
+const toggleBtn = document.querySelector(".nav-toggle");
+const toggleNavLink = document.querySelector(".nav-links");
+
+toggleBtn.addEventListener("click", () => {
+    toggleNavLink.classList.toggle("show")
+
+});
